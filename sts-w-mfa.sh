77eb97ff4c2d85501826a2348e832896bab9b2b8
NickTheSecurityDude <55202293+NickTheSecurityDude@users.noreply.github.com>
2021-05-18 21:09:10 -0500

    Add files via upload

--- sts-w-mfa.sh
+++ sts-w-mfa.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+set -e
 
 echo "Usage: source sts-w-mfa.sh <token-code>"
 
-if [[ $1 = '' ]];then
-  exit
+if [[ $1 != '' ]];then
+  TOKEN_CODE=$1
+
+  echo $TOKEN_CODE
+
+  TOKEN_FILE=token-output.txt
+
+  MFA_ARN=`aws sts get-caller-identity | jq -r .Arn |sed "s/:user\//:mfa\//"`
+
+  aws sts get-session-token --serial-number $MFA_ARN --token-code $TOKEN_CODE >$TOKEN_FILE || true
+  export AWS_ACCESS_KEY_ID=`cat $TOKEN_FILE |jq -r .Credentials.AccessKeyId`
+  export AWS_SECRET_ACCESS_KEY=`cat $TOKEN_FILE |jq -r .Credentials.SecretAccessKey`
+  export AWS_SESSION_TOKEN=`cat $TOKEN_FILE |jq -r .Credentials.SessionToken`
+  rm -f $TOKEN_FILE
+  aws sts get-caller-identity
+  echo $AWS_ACCESS_KEY_ID
 fi
 
-TOKEN_CODE=$1
-
-echo $TOKEN_CODE
-
-TOKEN_FILE=token-output.txt
-
-MFA_ARN=`aws sts get-caller-identity | jq -r .Arn |sed "s/:user\//:mfa\//"`
-
-aws sts get-session-token --serial-number $MFA_ARN --token-code $TOKEN_CODE >$TOKEN_FILE
-export AWS_ACCESS_KEY_ID=`cat $TOKEN_FILE |jq -r .Credentials.AccessKeyId`
-export AWS_SECRET_ACCESS_KEY=`cat $TOKEN_FILE |jq -r .Credentials.SecretAccessKey`
-export AWS_SESSION_TOKEN=`cat $TOKEN_FILE |jq -r .Credentials.SessionToken`
-rm -f $TOKEN_FILE
-aws sts get-caller-identity
-echo $AWS_ACCESS_KEY_ID
+true
